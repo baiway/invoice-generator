ce8a3ca24416101e60bc35b6e5560feb01ce4953
baiway <bailey.cook@sky.com>
2023-11-15 21:01:06 +0000

    Fix typo

--- src/calendar_api.py
+++ src/calendar_api.py
@@ -1,18 +1,16 @@
-import datetime
-from dateutil.parser import parse
 import os.path
-import json
 import re
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
+
 def authenticate_google_calendar():
-    """Setup the Google API authentication. 
+    """Setup the Google API authentication.
     Expects `credentials.json` in the `data` directory for the initial setup
     and uses `token.json` (also in the `data` directory) for subsequent authentication.
-    
+
     Returns a Google Calendar service object.
     """
     SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
@@ -24,36 +22,46 @@ def authenticate_google_calendar():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "data/credentials.json", SCOPES)
+                "data/credentials.json", SCOPES
+            )
             creds = flow.run_local_server(port=0)
         with open("data/token.json", "w") as token:
             token.write(creds.to_json())
-    i
     service = build("calendar", "v3", credentials=creds)
     return service
 
+
 def fetch_calendar_events(service, start_date, end_date):
-    """Fetch all Google Calendar events between a specified start and end date. 
+    """Fetch all Google Calendar events between a specified start and end date.
     Requires a Google Calendar service object and date range.
 
     Returns a list of events.
     """
     print("Fetching events...")
-    events_result = service.events().list(calendarId="primary", timeMin=start_date.isoformat(),
-                                          timeMax=end_date.isoformat(), singleEvents=True,
-                                          orderBy="startTime").execute()
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=start_date.isoformat(),
+            timeMax=end_date.isoformat(),
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
     return events_result.get("items", [])
+
 
 def process_events(events, student_data):
     """Processes a list of calendar events and categorises them based on student names.
     Uses regex to parse the event summary for student names, handling aliases as specified
     in `students.json`. Events that do not match the expected format are skipped.
-    
+
     Returns a dictionary with the following structure:
         lessons["student"] = [
             (lesson1_start_time, lesson1_end_time),
-            (lesson2_start_time, lesson2_end_time), 
-            ...   
+            (lesson2_start_time, lesson2_end_time),
+            ...
         ]
     where start_time and end_time are datetime objects.
     """
@@ -66,7 +74,7 @@ def process_events(events, student_data):
         # have booked a session under their name using my booking page.
         # If they do, the event is always titled "Tutoring (Firstname Lastname)"
         # Sometimes there are trailing spaces in the firstname and lastname fields;
-        # these are removed here. 
+        # these are removed here.
         match = re.search(r"\((.*?)\)", summary)
         if match:
             extracted_name = match.group(1).strip()  # extract and strip spaces
@@ -77,7 +85,7 @@ def process_events(events, student_data):
                     break
         elif summary.startswith("Tutoring "):
             student_name = summary.split("Tutoring ")[1].strip()
-    
+
         if not student_name:
             print(f"Skipping event with unexpected format: '{summary}'")
             continue
@@ -92,4 +100,3 @@ def process_events(events, student_data):
         lessons[student_name]["end"].append(end)
 
     return lessons
-
